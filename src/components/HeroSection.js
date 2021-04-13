8d1dd405982118c98aec176a1c18f8a7c6a76d53
Stackbit Code Editor <projects@stackbit.com>
2021-04-13 12:21:45 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -12,7 +12,7 @@ export default class HeroSection extends React.Component {
     }
 
     componentDidMount() {
-        fetch('https://swapi.dev/api/')
+        fetch('https://swapi.dev/api/planets/3/')
     }
     
 
